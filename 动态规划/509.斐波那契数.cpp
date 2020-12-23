aa7992ac2010a41d826045b9debec78fc4e450f1
周碧菡 <zhoubh@deepblueai.com>
2020-12-23 11:31:02 +0800

    add fibonacci

--- 动态规划/509.斐波那契数.cpp
+++ 动态规划/509.斐波那契数.cpp
@@ -5,10 +5,21 @@
  */
 
 // @lc code=start
+// 穷举-->备忘录-->
+#include<vector>
+using namespace std;
 class Solution {
 public:
     int fib(int n) {
-
+        if(n<1) return 0;
+        vector<int> mem(n+1,0);
+        return fibFunc(mem,n);
+    }
+    int fibFunc(vector<int>mem,int n){
+        if(n==1||n==2) return 1;
+        if(mem[n]!=0) return mem[n];
+        mem[n] = fibFunc(mem,n-1)+fibFunc(mem,n-2);
+        return mem[n];
     }
 };
 // @lc code=end
