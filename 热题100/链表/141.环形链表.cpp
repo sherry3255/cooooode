39e42e0e115c1a0e963b3d15030b9e2fd9bca4ed
周碧菡 <zhoubh@deepblueai.com>
2021-03-02 23:43:21 +0800

    add linkedList problems

--- 热题100/链表/141.环形链表.cpp
+++ 热题100/链表/141.环形链表.cpp
@@ -80,8 +80,29 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        
+        if(head == nullptr || head->next == nullptr){
+            return false;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(slow != fast){
+            if(fast == nullptr || fast->next == nullptr){
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return true;
     }
 };
+/*
+     时间复杂度：O(N)，其中 N 是链表中的节点数。
+
+    当链表中不存在环时，快指针将先于慢指针到达链表尾部，链表中每个节点至多被访问两次。
+
+    当链表中存在环时，每一轮移动后，快慢指针的距离将减小一。而初始距离为环的长度，因此至多移动 N 轮。
+
+    空间复杂度：O(1)。我们只使用了两个指针的额外空间。
+*/
 // @lc code=end
 
--- 热题100/链表/23.合并k个升序链表.cpp
+++ 热题100/链表/23.合并k个升序链表.cpp
@@ -73,9 +73,51 @@
  */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
 
+    ListNode* mergeTwoList(ListNode* l1,ListNode*l2){
+        ListNode* preHead = new ListNode(-1);
+        ListNode* prev = preHead;
+        while(l1!=nullptr && l2!=nullptr){
+            if(l1->val<l2->val){
+                prev->next = l1;
+                l1 = l1->next;
+            }else if(l2->val<l1->val){
+                prev->next = l2;
+                l2 = l2->next;
+            }
+            prev = prev->next;
+        }
+        prev->next = l1 == nullptr? l2:l1;
+        return preHead->next;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode* ans = nullptr;
+        for(size_t i = 0; i < lists.size(); ++i){
+            ans = mergeTwoList(ans, lists[i])
+        }
+        return ans;     
     }
 };
+/*
+时间复杂度：假设每个链表的最长长度是 n。在第一次合并后，ans 的长度为 n；第二次合并后，ans 的长度为 2×n，第 i 次合并后，ans 的长度为 i×n。第 i 次合并的时间代价是 O(n+(i−1)×n)=O(i×n)，那么总的时间代价为 O(∑i=1k(i×n))=O((1+k)⋅k2×n)=O(k2n)，故渐进时间复杂度为 O(k^2 n)。
+空间复杂度:没有用到与k和n 规模相关的辅助空间，故渐进空间复杂度为 O(1)。
+*/
+
+/* 分治算法在 mergeKLists上有区别
+ListNode* merge(vector<ListNode*> &list,int l, int r){
+    if(l==r) return lists[l];
+    if(l>r) return nullptr;
+    int mid = (l + r)>>1;
+    return mergeTwoList(merge(lists,l,mid),merge(lists,mid+1,r));
+}
+
+ListNode* mergeKLists(vector<ListNode*>& lists){
+    return merge(lists,0,lists.size()-1);
+}
+
+时间复杂度：考虑递归「向上回升」的过程——第一轮合并 k2\frac{k}{2}2k​ 组链表，每一组的时间代价是 O(2n)；第二轮合并 k/4 组链表，每一组的时间代价是 O(4n)......所以总的时间代价是 O(∑i=1~∞2^k/i×2^i×n)=O(kn×log⁡k)，故渐进时间复杂度为 O(kn×log⁡k)。
+空间复杂度：递归会使用到 O(log⁡k)O(\log k)O(logk) 空间代价的栈空间。
+
+*/
 // @lc code=end
 
