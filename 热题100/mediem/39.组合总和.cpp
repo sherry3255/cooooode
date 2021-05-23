5de2c08c2dc4001a905b83c5efd30cf93f746a30
sherry <sherrrrry>
2021-05-24 00:42:31 +0800

    update

--- 热题100/mediem/39.组合总和.cpp
+++ 热题100/mediem/39.组合总和.cpp
@@ -81,4 +81,20 @@ public:
     }
 };
 // @lc code=end
+/*
+时间复杂度：O(S)O(S)O(S)，其中 SSS 为所有可行解的长度之和。
+从分析给出的搜索树我们可以看出时间复杂度取决于搜索树所有叶子节点的深度之和，即所有可行解的长度之和。
+在这题中，我们很难给出一个比较紧的上界，我们知道 O(n×2n)O(n \times 2^n)O(n×2n) 是一个比较松的上界，
+即在这份代码中，nnn 个位置每次考虑选或者不选，如果符合条件，就加入答案的时间代价。
+但是实际运行的时候，因为不可能所有的解都满足条件，递归的时候我们还会用 target - candidates[idx] >= 0 进行剪枝，
+所以实际运行情况是远远小于这个上界的。
+
+空间复杂度：O(target)O(\textit{target})O(target)。
+除答案数组外，空间复杂度取决于递归的栈深度，在最差情况下需要递归 O(target)O(\textit{target})O(target) 层。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/combination-sum/solution/zu-he-zong-he-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
 
